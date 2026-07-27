1e6aff23da4cc64bba68885eafd637fa7e8640ec
Breo <breogannunezdiaz1980@gmail.com>
2026-07-27 22:52:16 +0100

    Add docstrings to Alert class and methods
    Added docstrings to the Alert class and its methods for better documentation.

--- Alert.py
+++ Alert.py
@@ -2,12 +2,27 @@ import cv2 as cv
 import numpy as np
 
 class Alert:
+    """
+    This class represents the alert of lane departure
+    """
     
     def __init__(self, filter):
+        """
+        Here wue initialize the object:
+
+        self.filter -> detector filter
+        self.frame_count -> a frame counter to prevent false positives.
+        """
+        
         self.filter = filter
         self.frame_count = 0
     
     def lane_departure_detector(self, area_detector):
+        """
+        This method verify if the frame has a line
+        """
+        
+        #We applay the detector filter
         detector = cv.bitwise_and(area_detector, self.filter)
         #We create a filter to discard the false positives
         pixels = cv.countNonZero(detector)
