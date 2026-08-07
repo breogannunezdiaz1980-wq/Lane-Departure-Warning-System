eec149e166544f0754e96de7882028bd1c9b83c4
Breo <breogannunezdiaz1980@gmail.com>
2026-08-07 19:59:58 +0100

    Modify edge detection logic in Filters.py
    Adjust edge detection based on median value of frame_gray. Implement CLAHE for low median values.

--- Filters.py
+++ Filters.py
@@ -27,9 +27,16 @@ class Filter_Lane_Departure:
         #BGR to gray scale
         frame_gray = cv.cvtColor(self.frame, cv.COLOR_BGR2GRAY)
         #We blur the image in order to remove the noise
-        frame_gray = cv.GaussianBlur(frame_gray, (7, 7), 0)
-        #Using the gradient we detect the edges
-        canny_edge = cv.Canny(frame_gray, 30, 90)
+        if np.median(frame_gray) > 80:
+            #Using the gradient we detect the edges
+            frame_gray = cv.GaussianBlur(frame_gray, (9, 9), 0)
+            canny_edge = cv.Canny(frame_gray, 30, 90)
+        else:
+            #Using the gradient we detect the edges
+            clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+            frame_gray = clahe.apply(frame_gray)
+            frame_gray = cv.GaussianBlur(frame_gray, (7, 7), 0)
+            canny_edge = cv.Canny(frame_gray, 10, 30)
         #We apply the draw triangle and the cleaner square to the frame
         roi_edge = cv.bitwise_and(canny_edge, self.filters[0])
         roi_edge = cv.bitwise_and(roi_edge, self.filters[2])
